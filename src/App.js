2162f22510cdc7488cb6e8868122ffa6e9ae33ff
Mark Liang <mliang@mozilla.com>
2018-03-01 00:34:28 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './components/header'
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
