9d9f7902a6772e2475fd8829871a72b60df0f2b5
Mark Liang <mliang@mozilla.com>
2018-03-15 00:42:30 +0800

    logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import logo from '../logo.jpg'
 
 export default () => (
   <footer className='tc pv4 pv5-ns bg-dark-black'>
-    <div className='logo center'></div>
+    <img src="images/g0v-logo.svg" width="199" height="32"/>
     <h2 className='f6 fw2 tracked moon-gray'>
     	<a href="#" className="u-line moon-gray dib relative">Grant 官網</a> • <a href="#" className="u-line moon-gray dib relative">g0v 官網</a> • <a href="#" className="u-line moon-gray dib relative">揪松網</a> • <a href="#" className="u-line moon-gray dib relative">收支明細</a>
     </h2>
