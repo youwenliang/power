70f41157497dd6a56cf9761b8be86831404102c4
Mark Liang <mliang@mozilla.com>
2018-05-07 21:33:29 +0800

    #

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <div id="language">
-            <a href="/power/en/index.html">EN</a><span>|</span><a href=".">中</a>
-          </div>
-          <div id="loading">
-            <figure className="o-0">
-              <img id="b2" src="images/butterfly/butterfly-move2.png" alt=""/>
-              <img id="b1" src="images/butterfly/butterfly-move1.png" alt=""/>
-            </figure>
-          </div>
           <Main/>
         </div>
     );
--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,22 @@ import Home from './Home'
 
 class Main extends Component {
   render() {
+    function changeLAN() {
+      if (typeof window !== 'undefined') {
+        window.location = '/power/en';
+      }
+    }
     return (
       <main>
+        <div id="language">
+          <a onClick={changeLAN} href="#">EN</a><span>|</span><a href=".">中</a>
+        </div>
+        <div id="loading">
+          <figure className="o-0">
+            <img id="b2" src="images/butterfly/butterfly-move2.png" alt=""/>
+            <img id="b1" src="images/butterfly/butterfly-move1.png" alt=""/>
+          </figure>
+        </div>
         <Switch>
           <Route exact path='/power' component={Home} />
         </Switch>
