c2d3ee09d25fa227166c8e31a3ef6f0e0f32102c
Mark Liang <mliang@mozilla.com>
2018-11-27 18:10:29 +0800

    add tooltips

--- src/components/partner.js
+++ src/components/partner.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Tooltip from 'rc-tooltip'
 
 var section =
 {
@@ -38,32 +39,39 @@ var partnerListData = [
     "image": ["images/partners/logo-ocf.svg"]
   },
   {
-    "name": "Chunghwa Telecom",
-    "description": "",
+    "name": "中華電信",
+    "description": "中華電信公司是國內最大的綜合電信業者，也是國際間電信業者重要合作伙伴，在促進全球化即時訊息溝通、全面提高社會大眾生活品質，以及普遍增強經濟社會運作效率上， 績效卓著。",
     "image": ["images/partners/logo-cht.png"]
   },
   {
     "name": "AppWorks",
-    "description": "",
+    "description": "AppWorks 是亞洲最大的創業加速器，自 2010 年成立以來，協助網路創業者結合線上與實體的活動，以自由開放、跨界社群、多邊交流精神，打造台灣最大的網路創業社群。",
     "image": ["images/partners/logo-appworks.png"]
   },
   {
-    "name": "EasyCard Corp.",
-    "description": "",
+    "name": "悠遊卡公司",
+    "description": "悠遊卡公司正式成立於 2000 年 3 月， 非接觸式 IC 智慧卡「悠遊卡」從交通運輸、小額消費、行政規費和風景區門票等，提供民眾在食、衣、住、行各方面便利的支付工具，創造更美好的生活。",
     "image": ["images/partners/logo-easycard.png"]
   }
 ]
 
 class Partner extends Component {
   partner = (i) => {
+    var ratio = {
+      objectFit: "contain"
+    }
     return (
-      <div className="fl partners ma3 bg-white hidediv hideme br1">
-        <div className="pa4">
-          <figure className="center mb3">
-            <img src={partnerListData[i].image[0]} height="100" alt={partnerListData[i].name} />
-          </figure>
+      <div className="fl partners ma3 bg-white hidediv hideme br1 relative">
+        <div className="pa4 relative">
+          <Tooltip placement="bottom" mouseLeaveDelay={0} trigger={['hover']} overlay={<span><span class='fw7 f5 mv3'>{partnerListData[i].name}</span><br/>{partnerListData[i].description}</span>}>
+            <figure className="center mb3 tc">
+              <img src={partnerListData[i].image[0]} height="100" alt={partnerListData[i].name} style={ratio}/>
+            </figure>
+          </Tooltip>
+          {/*
           <p className="ma0 tracked o-90 pt3 fw6">{partnerListData[i].name}</p>
           <p className="mw-320 center o-70 p-small ph2">{partnerListData[i].description}</p>
+          */}
         </div>
       </div>
     )
@@ -79,7 +87,7 @@ class Partner extends Component {
           <div className="mw8 w-85 center">
             <div className="cf">
               <div className="fl w-25-l w-100 ma0 pa3 tc-ns hideme hidediv">
-                <img src="images/powered-by-g0v.svg" height="57" alt="g0v-poweredby" />
+                <img src="images/powered-by-g0v.svg" height="57" alt="g0v-poweredby"/>
               </div>
               <div className="fl w-75-l w-100 ma0 pa3 hideme hidediv white">
                 <p className="fw4 ma0 pa0 lh-large">{section.powerby}</p>
